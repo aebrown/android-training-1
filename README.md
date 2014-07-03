715982a889b8f7ae65e05961dc25d5dc6adbd38b
Alan Brown <abrown@familysearch.org>
2014-07-03 08:00:14 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+training-1
+============
+Source for the Android Training activity described in [Building Your First App](http://developer.android.com/training/basics/firstapp/index.html). Each step is a separate branch, named after the title of the step.
---
+++ src/com/example/myapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu items for use in the action bar
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_activity_actions, menu);
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle presses on the action bar items
+    switch (item.getItemId()) {
+      case R.id.action_search:
+        openSearch();
+        return true;
+      case R.id.action_settings:
+        openSettings();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  private void openSearch() {
+    showToast(R.string.action_search);
+  }
+
+  private void openSettings() {
+    showToast(R.string.action_settings);
+  }
+
+  private void showToast(int id) {
+    Toast toast = Toast.makeText(this, getResources().getString(id), Toast.LENGTH_LONG);
+    toast.show();
+  }
+
+}
